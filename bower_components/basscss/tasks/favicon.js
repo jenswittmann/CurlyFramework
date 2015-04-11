defcf10dc505f4f1c70173820390508921c4a91f
Jens Wittmann <post@jens-wittmann.de>
2015-04-12 01:37:33 +0200

    BASSCSS Update
    Update BASSCSS Framework to current version.

--- bower_components/basscss/tasks/favicon.js
+++
@@ -1,8 +0,0 @@
-
-var svgtopng = require('svg-to-png');
-
-module.exports = function() {
-  svgtopng.convert('./docs/src/svg/favicon.svg', './docs/images', {});
-  svgtopng.convert('./docs/src/svg/apple-touch-icon.svg', './docs/images', {});
-  svgtopng.convert('./docs/src/svg/built-badge.svg', './docs/images', {});
-};
