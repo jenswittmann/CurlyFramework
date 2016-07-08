0268868774385f985e550ce1769546cbb1c55b3c
Jens Wittmann <post@jens-wittmann.de>
2016-07-08 14:03:33 +0200

    update js

--- js/work/script.work.js
+++ js/work/script.work.js
@@ -13,42 +13,42 @@ $(document).ready(function() {
 	}
 	detectMobile();
 	
-	// Slick Slider
-	$('.slick').slick({
-		dots: true,
-		infinite: false,
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		prevArrow: '',
-		nextArrow: '',
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true,
-					arrows: false
-				}
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2,
-					arrows: false
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					arrows: false
-				}
-			}
-		]
+	// mobile nav
+	var nav = $('.head-nav');
+	var btn = $('.head-nav-open');
+	btn.on('click', function(e) {
+		e.preventDefault();
+		if (nav.hasClass('active')) {
+			nav.removeClass('active');
+		} else {
+			nav.addClass('active');
+		}
 	});
 	
+	// slick slider
+	$('.slick').slick({
+		arrows: false,
+		dots: true
+	});	
+		
+	// modaal window
+	$('.img-caption a').modaal({
+		type: 'image'
+	});					   
+	
+	// scrollto
+	$('.scrollto').on('click', function(e) {
+		e.preventDefault();
+		var offset = 20;
+		if (mobile == 0) {
+			offset = 100;
+		}
+		var anchor = $(this).data('scrollto');
+		$('html,body').animate({
+			scrollTop: $(anchor).offset().top - offset
+		}, 500, function() {
+			window.location.hash = anchor;
+		});
+	});
+		
 });
