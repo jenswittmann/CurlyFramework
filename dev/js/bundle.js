0db3b54a773ec79ed2c29a54973b01e03de5c422
Jens Wittmann <post@jens-wittmann.de>
2024-05-12 23:47:55 +0200

    docs: add accordion example with @alpinejs/collapse

--- dev/js/bundle.js
+++ dev/js/bundle.js
@@ -2,9 +2,11 @@ import "lazysizes";
 import Alpine from "alpinejs";
 import focus from "@alpinejs/focus";
 import intersect from "@alpinejs/intersect";
+import collapse from "@alpinejs/collapse";
 
 Alpine.plugin(focus);
 Alpine.plugin(intersect);
+Alpine.plugin(collapse);
 
 window.Alpine = Alpine;
 
