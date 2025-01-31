c7a9aeb1b6d7fddefc1c25b8ee3a1893f7c8864f
Jens Wittmann <post@jens-wittmann.de>
2025-01-31 11:04:16 +0100

    add darkmode

--- dev/js/bundle.js
+++ dev/js/bundle.js
@@ -3,10 +3,12 @@ import Alpine from "alpinejs";
 import focus from "@alpinejs/focus";
 import intersect from "@alpinejs/intersect";
 import collapse from "@alpinejs/collapse";
+import persist from "@alpinejs/persist";
 
 Alpine.plugin(focus);
 Alpine.plugin(intersect);
 Alpine.plugin(collapse);
+Alpine.plugin(persist);
 
 window.Alpine = Alpine;
 
